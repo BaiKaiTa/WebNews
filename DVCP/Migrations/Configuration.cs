3f5eb08e4a486b1a086d75aaeea7bdedc2f5a4fb
ByKyTa <bykyta@outlook.com.vn>
2021-07-20 09:29:01 +0700

    edit TagName

--- DVCP/Migrations/Configuration.cs
+++ DVCP/Migrations/Configuration.cs
@@ -23,9 +23,9 @@
                 new WebInfo
                 {
                     id = 1,
-                    web_name = "Đại Việt Cổ Phong",
-                    web_des = "Trang web chính thức của Đại Việt Cổ Phong",
-                    web_about = "Về Đại Việt Cổ Phong",
+                    web_name = "7-Màu",
+                    web_des = "Trang web tin tức chính thức của 7-Màu",
+                    web_about = "Về 7-Màu",
                 }
                 );
             context.Users.AddOrUpdate(x => x.username,
@@ -33,21 +33,15 @@
                 {
                     username = "admin",
                     password = "0192023A7BBD73250516F069DF18B500", // = admin123
-                    fullname = "ADMIN ĐVCP",
+                    fullname = "ADMIN News",
                     userrole = "admin",
                     status = true,
                 }
                 );
             context.Tags.AddOrUpdate(x => x.TagID,
-                new Tag { TagID = 1, TagName = "Kiến trúc" },
-                new Tag { TagID = 2, TagName = "Chất liệu" },
-                new Tag { TagID = 3, TagName = "Binh bị" },
-                new Tag { TagID = 4, TagName = "Quân sự" },
-                new Tag { TagID = 5, TagName = "Thần thoại" },
-                new Tag { TagID = 6, TagName = "Văn hóa" },
-                new Tag { TagID = 7, TagName = "Phong tục" },
-                new Tag { TagID = 8, TagName = "Tôn giáo" },
-                new Tag { TagID = 9, TagName = "Trang phục" }
+                new Tag { TagID = 1, TagName = "Game PC" },
+                new Tag { TagID = 2, TagName = "Game Moblie" },
+                new Tag { TagID = 3, TagName = "Game Console" }
                 );
             context.SaveChanges();
         }
